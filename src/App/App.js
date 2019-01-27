7bb73c350ee79934e0bf95cb9d7280942af3be87
Braedon <braedongough@gmail.com>
2019-01-27 12:02:13 +0100

    added border and updated background color

--- src/App/App.js
+++ src/App/App.js
@@ -27,31 +27,81 @@ class App extends Component {
   };
   render() {
     return (
-      <div>
+      <>
+        <Logo
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2000px-International_Pok%C3%A9mon_logo.svg.png"
+          alt="Pokemon Logo"
+        />
         <Container>
-          <p>{this.state.name}</p>
-          <img src={this.state.sprite} alt={this.state.name} />
-          <div>
-            {this.state.stats.map(({ stat, base_stat }, index) => (
-              <p key={index}>
-                {stat.name}: {base_stat}
-              </p>
-            ))}
-          </div>
-          <button onClick={this.handleClick}>Click here for good stuff</button>
+          <Name>{this.state.name}</Name>
+          <SpriteContainer>
+            <Sprite src={this.state.sprite} alt={this.state.name} />
+            <div>
+              {this.state.stats.map(({ stat, base_stat }, index) => (
+                <p key={index}>
+                  <span className="stat-name">{stat.name}:</span> {base_stat}
+                </p>
+              ))}
+            </div>
+          </SpriteContainer>
+          <Button onClick={this.handleClick}>New Pokemon</Button>
         </Container>
-      </div>
+      </>
     );
   }
 }
+
+const Logo = styled.img`
+  width: 350px;
+`;
 
 const Container = styled.div`
   display: grid;
   grid-template-rows: auto 1fr auto;
   justify-items: center;
-  background: #000;
-  color: white;
-  width: 96px;
+  padding: 10px;
+  grid-gap: 20px;
+  border: 3px solid black;
+  border-radius: 5px;
+  background: white;
+`;
+
+const SpriteContainer = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 128px 1fr;
+  .stat-name {
+    font-weight: 700;
+  }
+`;
+
+const Sprite = styled.img`
+  margin: 30px auto;
+  padding: 0;
+  background: white;
+  align-self: center;
+  width: 128px;
+`;
+
+const Name = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+const Button = styled.button`
+  margin: 10px auto;
+  padding: 10px;
+  border-width: 0;
+  outline: none;
+  border-radius: 2px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.6);
+  background-color: #2ecc71;
+  color: #ecf0f1;
+  transition: background-color 0.3s;
+  &:hover,
+  &:focus {
+    background-color: #27ae60;
+  }
 `;
 
 export default App;
--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: `https://pokeapi.co/api/v2/pokemon`
 });
 
-const randomNum = () => Math.floor(Math.random() * 150 + 1).toString();
+const randomNum = () => Math.floor(Math.random() * 807 + 1).toString();
 
 export default () => {
   return api.get(randomNum());
